14fa5ce65ca5d33e9f83f25df9e0b99bd55ee5cd
cafehaine <kilian.guillaume@gmail.com>
2019-09-14 11:24:05 +0200

    Add aurgraph.py

---
+++ aurgraph.py
@@ -0,0 +1,54 @@
+"""
+A small cli tool to generate dependency graphs for the aur.
+
+Depends on python requests, networkx and pydot.
+
+To generate a png of the graph you can use the following command:
+dot -Tpng graph.dot -ograph.png
+"""
+
+from argparse import ArgumentParser
+
+from requests import get
+from networkx import DiGraph
+from networkx.drawing.nx_pydot import write_dot
+
+AUR_RPC_URL = "https://aur.archlinux.org/rpc/"
+
+class Query:
+    def __init__(self, parent, relation, args):
+        self.parent = parent
+        self.relation = relation
+        self.args = args
+        self.args['v'] = 5
+        self.args['type'] = 'search'
+
+parser = ArgumentParser(description='Generate a graph of the aur packages that depend on ones maintained by a maintainer.')
+parser.add_argument('--maintainer', nargs='?', default='cafehaine', help='the maintainer')
+
+args = parser.parse_args()
+
+G = DiGraph()
+
+queries = [
+    Query(None, None, {'by': 'maintainer', 'arg': args.maintainer})
+]
+
+while queries:
+    query = queries.pop()
+    r = get(AUR_RPC_URL, params=query.args)
+    json = r.json()
+    print(json)
+    for package in json['results']:
+        name = package['Name']
+        if package['Maintainer'] == args.maintainer:
+            G.add_node(name, color='red')
+        else:
+            G.add_node(name)
+        if query.parent:
+            G.add_edge(query.parent, name)
+        queries.append(Query(name, 'depends', {'by': 'depends', 'arg': name}))
+        queries.append(Query(name, 'makedepends', {'by': 'makedepends', 'arg': name}))
+        queries.append(Query(name, 'checkdepends', {'by': 'checkdepends', 'arg': name}))
+
+write_dot(G, 'graph.dot')
