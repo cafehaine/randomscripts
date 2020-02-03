3c66288b6652f8bb08570fdc9878ea25c5e9292f
cafehaine <kilian.guillaume@gmail.com>
2020-02-03 22:24:22 +0100

    Pixel Flasher: disable unused modules

---
+++ pixel flasher/conf.lua
@@ -0,0 +1,23 @@
+function love.conf(t)
+	t.window.resizable = true
+	t.window.title = "Pixel Flasher"
+
+	t.modules.audio = false
+	t.modules.data = false
+	t.modules.event = true
+	t.modules.font = false
+	t.modules.graphics = true
+	t.modules.image = false
+	t.modules.joystick = false
+	t.modules.keyboard = true
+	t.modules.math = false
+	t.modules.mouse = true
+	t.modules.physics = false
+	t.modules.sound = false
+	t.modules.system = false
+	t.modules.thread = false
+	t.modules.timer = true
+	t.modules.touch = true
+	t.modules.video = false
+	t.modules.window = true
+end
